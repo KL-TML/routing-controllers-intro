6e69437cef7605f3afdc70d0c8d27bf3dea5fb8d
KYLE LIPTON <lipton.kyle@gmail.com>
2017-11-21 19:27:28 -0500

    added secret page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   before_action :set_kitten_url, only: [:kitten, :kittens]
 
   def welcome
-   @header = "this is the welcome page"
+   @header = "this is a header"
   end
 
   def about
@@ -22,6 +22,13 @@ class PagesController < ApplicationController
   def set_kitten_url
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+
+  def secrets
+    if params[:magic_word] != "bitmaker"
+      flash[:alert] = "Sorry, you're not authorized to see that page!"
+      redirect_to '/welcome'
+    end
   end
 
 end
